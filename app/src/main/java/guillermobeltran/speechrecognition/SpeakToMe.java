c7f70b7780295f2982571cb4814747e3e42ec0f2
memobelt <guillermo_beltran@brown.edu>
2015-05-28 15:37:14 -0400

    http://www.androidhive.info/2014/07/android-speech-to-text-tutorial/
    Followed instructions from above

--- app/src/main/java/guillermobeltran/speechrecognition/SpeakToMe.java
+++ app/src/main/java/guillermobeltran/speechrecognition/SpeakToMe.java
@@ -1,128 +1,92 @@
 package guillermobeltran.speechrecognition;
 
-import android.content.Context;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import static guillermobeltran.speechrecognition.R.id.imageButton;
 
 
-public class SpeakToMe extends ActionBarActivity {
+public class SpeakToMe extends Activity {
+
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speak_to_me);
-        final Button button = (Button) findViewById(R.id.button);
-        final Button button2 = (Button) findViewById(R.id.button2);
-        final SpeechRecognizer speech = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
-        button.setOnClickListener(new View.OnClickListener() {
+
+        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        btnSpeak = (ImageButton) findViewById(imageButton);
+
+        // hide the action bar
+//        getActionBar().hide();
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
             public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence passed = "Speech Recognition passed";
-                CharSequence failed = "Speech Recognition failed";
-                int duration = Toast.LENGTH_SHORT;
-                if(SpeechRecognizer.isRecognitionAvailable(context)){
-                    Toast toast = Toast.makeText(context, passed, duration);
-                    toast.show();
-                    Listen(context, speech);
-                }
-                else{
-                    Toast toast = Toast.makeText(context, failed, duration);
-                    toast.show();
-                }
+                promptSpeechInput();
             }
         });
-        button2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                speech.stopListening();
-            }
-        });
+
     }
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    public void Listen(Context context, SpeechRecognizer speech){
-        Intent rec = RecognizerIntent.getVoiceDetailsIntent(context);
-        RecognitionListener listener = new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle params) {
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
 
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-
-            }
-
-            @Override
-            public void onRmsChanged(float rmsdB) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] buffer) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-
-            }
-
-            @Override
-            public void onError(int error) {
-
-            }
-
-            @Override
-            public void onResults(Bundle results) {
-            ArrayList<String> voiceResults = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            StringBuilder sb = new StringBuilder();
-                for(String p: voiceResults){
-                    sb.append(p);
-                    sb.append("\n");
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
                 }
-                Toast te = new Toast(getApplicationContext());
-                te.setText(sb.toString());
-                te.setDuration(Toast.LENGTH_SHORT);
-                te.show();
+                break;
             }
 
-            @Override
-            public void onPartialResults(Bundle partialResults) {
-
-            }
-
-            @Override
-            public void onEvent(int eventType, Bundle params) {
-
-            }
-        };
-        speech.setRecognitionListener(listener);
-        Intent recognizerIntent = RecognizerIntent.getVoiceDetailsIntent(getApplicationContext());
-        speech.startListening(recognizerIntent);
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_speak_to_me, menu);
+        return true;
     }
+
 }
