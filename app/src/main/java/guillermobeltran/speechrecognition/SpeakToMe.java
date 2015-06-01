16f71b322c8bdd6a2559ea57859953d2fca6f0c9
memobelt <guillermo_beltran@brown.edu>
2015-06-01 13:50:05 -0400

    Deleted CameraPreview and OpenCamera. Moved OpenCamera code to SpeakToMe. Taking a picture with the back camera works in emulator but not in G2

--- app/src/main/java/guillermobeltran/speechrecognition/SpeakToMe.java
+++ app/src/main/java/guillermobeltran/speechrecognition/SpeakToMe.java
@@ -1,23 +1,34 @@
 package guillermobeltran.speechrecognition;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.media.Image;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import static guillermobeltran.speechrecognition.R.id.imageButton;
+import static guillermobeltran.speechrecognition.R.id.imageButton2;
 
 
 public class SpeakToMe extends Activity {
@@ -26,6 +37,11 @@ public class SpeakToMe extends Activity {
     private ImageButton btnSpeak;
     private ImageButton btnCam;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 200;
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 300;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    private Uri fileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +50,7 @@ public class SpeakToMe extends Activity {
         //Looks for the txtSpeechInput
         txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
         btnSpeak = (ImageButton) findViewById(imageButton);
+        btnCam = (ImageButton) findViewById(imageButton2);
 
         // Crashed the app so commented it out
 //        getActionBar().hide();
@@ -82,12 +99,83 @@ public class SpeakToMe extends Activity {
                 }
                 break;
             }
-
+            case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE: {
+                if (resultCode == RESULT_OK) {
+                    // Image captured and saved to fileUri specified in the Intent
+                    btnCam.setImageURI(fileUri);
+                    btnCam.setScaleType(ImageView.ScaleType.FIT_XY);
+                } else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Operation failed\n", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Operation failed\n", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+            case CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE: {
+                if (resultCode == RESULT_OK) {
+                    // Video captured and saved to fileUri specified in the Intent
+                    Toast.makeText(this, "Video saved to:\n" +
+                            data.getData(), Toast.LENGTH_LONG).show();
+                } else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Operation failed\n", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Operation failed\n", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
         }
     }
 
     public void takePic(View view){
-        Intent intent = new Intent(this, OpenCamera.class);
-        startActivity(intent);
+        Context context = this;
+        PackageManager packageManager = context.getPackageManager();
+        if(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY) == false) {
+            Toast.makeText(this, "This device does not have a camera.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+            // start the image capture Intent
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), ".SpeechRecog");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("SpeechRecog", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
 }
