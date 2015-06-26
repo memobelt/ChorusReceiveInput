2d1572347e70410333fa04749fd680aa9e6bdbc5
sumkit <skitahar@andrew.cmu.edu>
2015-06-25 22:44:42 -0400

    can't figure out why github hates me and will not push everything, watch and phone apps on same project, open on phone from watch (not finalized though)

--- app/src/main/java/guillermobeltran/chorusinput/ListenerServiceFromWear.java
+++ app/src/main/java/guillermobeltran/chorusinput/ListenerServiceFromWear.java
@@ -2,29 +2,57 @@ package guillermobeltran.chorusinput;
 
 import android.content.Intent;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.WearableListenerService;
 
 public class ListenerServiceFromWear extends WearableListenerService {
-
-    private static final String ANSWER_ON_PHONE = "/answer-on-phone";
-
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         Log.i("test", "onMessageReceived()");
-        Toast.makeText(getApplicationContext(),"onMessageReceived",Toast.LENGTH_LONG);
         /*
          * Receive the message from wear
          */
-        if (messageEvent.getPath().equals(ANSWER_ON_PHONE)) {
+        //open on phone was called from MainActivity to answer
+        if (messageEvent.getPath().equals("/main-activity-on-phone")) {
             Intent startIntent = new Intent(this, ChorusChat.class);
+            startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startIntent);
+        }
+        //open on phone was called from microphone to take a picture
+        else if(messageEvent.getPath().equals("/microphone-on-phone")) {
+            Intent startIntent = new Intent(this, SpeakToMe.class);
             startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(startIntent);
         }
         else {
             super.onMessageReceived(messageEvent);
+            Log.i("test", "Message path does not match");
         }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("test","start");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("test","create");
+    }
+
+    @Override
+    public void onPeerConnected(Node peer) {
+        super.onPeerConnected(peer);
+        Log.i("test",peer.getDisplayName());
+    }
+
+    @Override
+    public void onPeerDisconnected(Node peer) {
+        super.onPeerDisconnected(peer);
+        Log.i("test",peer.getDisplayName());
     }
 }
