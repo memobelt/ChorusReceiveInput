eef3e3e0da50a2d8985ca4c58317199977c8f37e
Jason Song <jiachens@andrew.cmu.edu>
2015-06-26 03:10:20 -0400

    login & signup template

---
+++ app/src/main/java/guillermobeltran/chorusinput/LoginManager/main/AppConfig.java
@@ -0,0 +1,14 @@
+package guillermobeltran.chorusinput.LoginManager.main;
+
+public class AppConfig {
+    // Server user login url
+//    public static String URL_LOGIN = "http://192.168.0.101/android_login_api/";
+    public static String URL_LOGIN = "http://192.168.1.4:8888/android_login_api/";
+
+
+    // Server user register url
+//    public static String URL_REGISTER = "http://192.168.0.101/android_login_api/";
+    public static String URL_REGISTER = "http://192.168.1.4:8888/android_login_api/";
+
+
+}
---
+++ backend/android_login_api/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "js8383");
+define("DB_DATABASE", "android_api");
+?>
