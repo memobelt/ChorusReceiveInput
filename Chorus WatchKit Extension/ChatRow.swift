2662c88d376c863fcf3aaafdefb32767b0984334
Jason Song <jiachens@andrew.cmu.edu>
2015-08-25 21:08:52 -0400

    watch template

---
+++ Chorus WatchKit Extension/ChatRow.swift
@@ -0,0 +1,15 @@
+//
+//  ChatRow.swift
+//  Chorus
+//
+//  Created by Jiachen Song on 8/25/15.
+//  Copyright (c) 2015 Summer Kitahara. All rights reserved.
+//
+
+import WatchKit
+
+class ChatRow: NSObject {
+    @IBOutlet weak var titleLabel: WKInterfaceLabel!
+    @IBOutlet weak var detailLabel: WKInterfaceLabel!
+    
+}
